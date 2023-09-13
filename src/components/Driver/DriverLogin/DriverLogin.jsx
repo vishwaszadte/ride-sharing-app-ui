88c04a5fa42bd42e6a44baaa16a1306678df7b66
Vishwas <vishwaszadte@gmail.com>
2023-09-13 20:51:24 +0530

    Import useEffect

--- src/components/Driver/DriverLogin/DriverLogin.jsx
+++ src/components/Driver/DriverLogin/DriverLogin.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./DriverLogin.css";
 import axios from "axios";
+import { useEffect } from "react";
 
 const DriverLogin = () => {
   const [email, setEmail] = useState("");
